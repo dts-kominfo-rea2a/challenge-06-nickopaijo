7288d0583dd9a06090642600944321d2b56bcab6
nickopaijo <nickopaijo@gmail.com>
2022-06-08 16:50:55 +0700

    answer challenge 06

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,30 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf-8", (err, data) => {
+    const data1 = JSON.parse(data);
+    const message1 = getLastWord(data1.message);
+
+    fs.readFile(file2, "utf-8", (err, data) => {
+      const data2 = JSON.parse(data);
+      const message2 = getLastWord(data2[0].message);
+
+      fs.readFile(file3, "utf-8", (err, data) => {
+        const data3 = JSON.parse(data);
+        const message3 = getLastWord(data3[0].data.message);
+
+        fnCallback(err, [message1, message2, message3]);
+      });
+    });
+  });
+};
+
+const getLastWord = (sentence) => {
+  const arrSentence = sentence.split(" ").reverse();
+
+  return arrSentence[0];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
